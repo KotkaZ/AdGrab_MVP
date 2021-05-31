49b09d22c74d0a669a5bc968fb16ba52e504ef6f
Allan Loo <loo_allan@yahoo.com>
2021-05-31 20:31:42 +0300

    Revert "Added PrimeVue depen."
    This reverts commit fade2c67c7de5f327e847a85405286ce0dbbbd70.

--- plugins/primevue.js
+++
@@ -1,4 +0,0 @@
-import 'primeflex/primeflex.css'
-import 'primevue/resources/themes/saga-blue/theme.css' //theme
-import 'primevue/resources/primevue.min.css' //core css
-import 'primeicons/primeicons.css' //icons
