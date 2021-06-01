3057e6476210e3086a732e759f476b2467f0ddd1
Kerdo Kurs <kerdokurs@gmail.com>
2021-06-01 12:11:52 +0300

    add basic getting started page

---
+++ src/views/Start.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-grid p-p-4">
+    <div class="p-col-1"></div>
+    <div class="p-col-3">
+      <Card>
+        <template #content> Telereklaam </template>
+      </Card>
+    </div>
+    <div class="p-col-4">
+      <Card>
+        <template #content> Trükimeedia </template>
+      </Card>
+    </div>
+    <div class="p-col-3">
+      <Card>
+        <template #content> Veebireklaam </template>
+      </Card>
+    </div>
+    <div class="p-col-1"></div>
+  </div>
+</template>
+
+<script>
+import Card from "primevue/card";
+
+export default {
+  components: {
+    Card,
+  },
+  setup() {},
+};
+</script>
