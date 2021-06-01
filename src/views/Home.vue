632c965ab6b853bc68e189fb43fe0016470eb1dd
Kerdo Kurs <kerdokurs@gmail.com>
2021-06-01 16:21:27 +0300

    add redirect to catalog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,16 @@
     </div>
     <div>
       <HelloWorld msg="Welcome to AdGrab" />
-      <HelloWorld side-msg="AdGrab is a startup offering easy advertisement solutions." />
-      <Button label="DEMO" icon="pi pi-check" iconPos="right" class="p-button-lg" v-on:click="$router.push('demo')"/>
+      <HelloWorld
+        side-msg="AdGrab is a startup offering easy advertisement solutions."
+      />
+      <Button
+        label="DEMO"
+        icon="pi pi-check"
+        iconPos="right"
+        class="p-button-lg"
+        v-on:click="$router.push('catalog')"
+      />
     </div>
     <div class="p-col-12 p-m-6 p-grid p-jc-around p-ai-center">
       <Card class="roundedCard p-shadow-6 p-col-2">
@@ -15,11 +23,14 @@
         </template>
         <template #content>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat tellus felis, id maximus dui
-            feugiat eget. Sed lacus libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus mi, accumsan quis
-            ornare ut, euismod sit amet metus. Cras a turpis metus. Praesent in lobortis massa, non malesuada tellus.
-            Aliquam nec vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel est ultricies faucibus.
-            Vivamus feugiat egestas tempor. Vivamus in odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            volutpat tellus felis, id maximus dui feugiat eget. Sed lacus
+            libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus
+            mi, accumsan quis ornare ut, euismod sit amet metus. Cras a turpis
+            metus. Praesent in lobortis massa, non malesuada tellus. Aliquam nec
+            vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel
+            est ultricies faucibus. Vivamus feugiat egestas tempor. Vivamus in
+            odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
             Donec auctor sed elit a tempus.
           </p>
         </template>
@@ -29,13 +40,16 @@
           <img src="../assets/card2.jpeg" style="height: 15rem" />
         </template>
         <template #content>
-          <p>p-jc-center
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat tellus felis, id maximus dui
-            feugiat eget. Sed lacus libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus mi, accumsan quis
-            ornare ut, euismod sit amet metus. Cras a turpis metus. Praesent in lobortis massa, non malesuada tellus.
-            Aliquam nec vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel est ultricies faucibus.
-            Vivamus feugiat egestas tempor. Vivamus in odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
-            Donec auctor sed elit a tempus.
+          <p>
+            p-jc-center Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aliquam volutpat tellus felis, id maximus dui feugiat eget. Sed
+            lacus libero, finibus sed elit nec, tempus viverra risus. Vivamus
+            lacus mi, accumsan quis ornare ut, euismod sit amet metus. Cras a
+            turpis metus. Praesent in lobortis massa, non malesuada tellus.
+            Aliquam nec vulputate massa. Donec dignissim rutrum metus. Curabitur
+            a felis vel est ultricies faucibus. Vivamus feugiat egestas tempor.
+            Vivamus in odio vitae eros elementum sagittis. Aenean vel imperdiet
+            dolor. Donec auctor sed elit a tempus.
           </p>
         </template>
       </Card>
@@ -45,11 +59,14 @@
         </template>
         <template #content>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat tellus felis, id maximus dui
-            feugiat eget. Sed lacus libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus mi, accumsan quis
-            ornare ut, euismod sit amet metus. Cras a turpis metus. Praesent in lobortis massa, non malesuada tellus.
-            Aliquam nec vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel est ultricies faucibus.
-            Vivamus feugiat egestas tempor. Vivamus in odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            volutpat tellus felis, id maximus dui feugiat eget. Sed lacus
+            libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus
+            mi, accumsan quis ornare ut, euismod sit amet metus. Cras a turpis
+            metus. Praesent in lobortis massa, non malesuada tellus. Aliquam nec
+            vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel
+            est ultricies faucibus. Vivamus feugiat egestas tempor. Vivamus in
+            odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
             Donec auctor sed elit a tempus.
           </p>
         </template>
@@ -60,11 +77,14 @@
         </template>
         <template #content>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam volutpat tellus felis, id maximus dui
-            feugiat eget. Sed lacus libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus mi, accumsan quis
-            ornare ut, euismod sit amet metus. Cras a turpis metus. Praesent in lobortis massa, non malesuada tellus.
-            Aliquam nec vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel est ultricies faucibus.
-            Vivamus feugiat egestas tempor. Vivamus in odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            volutpat tellus felis, id maximus dui feugiat eget. Sed lacus
+            libero, finibus sed elit nec, tempus viverra risus. Vivamus lacus
+            mi, accumsan quis ornare ut, euismod sit amet metus. Cras a turpis
+            metus. Praesent in lobortis massa, non malesuada tellus. Aliquam nec
+            vulputate massa. Donec dignissim rutrum metus. Curabitur a felis vel
+            est ultricies faucibus. Vivamus feugiat egestas tempor. Vivamus in
+            odio vitae eros elementum sagittis. Aenean vel imperdiet dolor.
             Donec auctor sed elit a tempus.
           </p>
         </template>
@@ -75,26 +95,25 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 import Button from 'primevue/button';
 import Card from 'primevue/card';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld,
     Button,
-    Card
+    Card,
   },
 };
-
 </script>
 <style lang="css">
-  .logo {
-    border-radius: 50%;
-    max-width: 7vw;
-  }
-  .roundedCard {
-    border-radius: 3%;
-  }
+.logo {
+  border-radius: 50%;
+  max-width: 7vw;
+}
+.roundedCard {
+  border-radius: 3%;
+}
 </style>
