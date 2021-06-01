119d341a9594c00366cec4a06275a737101e39eb
Kerdo Kurs <kerdokurs@gmail.com>
2021-06-01 13:48:35 +0300

    fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,42 @@
+<<<<<<< Updated upstream
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Demo from "../views/Demo.vue";
+=======
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+>>>>>>> Stashed changes
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: "/demo",
     name: "Demo",
     component: Demo,
-  }
+  },
+  {
+    path: '/get-started',
+    name: 'Get started',
+    component: () => import('../views/Start.vue'),
+  },
+  {
+    path: '/catalog',
+    name: 'Catalog',
+    component: () => import('../views/Catalog.vue'),
+  },
 ];
 
 const router = createRouter({
