0ac541d19a606ba31ce3e8b8c5427a8c89260468
Karl Rapur <rapurkarl@gmail.com>
2021-06-01 18:10:19 +0300

    Implemented Cart

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -7,7 +7,7 @@
             </template>
 
             <template #right>
-                <Button icon="pi pi-shopping-cart" class="p-button-text p-mr-2" />
+                <Button icon="pi pi-shopping-cart" class="p-button-text p-mr-2" @click="this.$router.push('/cart')" />
                 <Button class="p-button-text" type="button" icon="pi pi-bars" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"/>
                 <Menu id="overlay_menu" ref="menu" :model="items" :popup="true" />
             </template>
@@ -17,42 +17,47 @@
 
 <script>
 import Toolbar from 'primevue/toolbar';
-import Button from 'primevue/button'
+import Button from 'primevue/button';
 import Menu from 'primevue/menu';
 export default {
-components: { Toolbar,Button, Menu},
-data() {
-		return {
-			items: [
-				{
-					label: 'Active Campaigns',
-					icon: 'pi pi-flag',
-				},
-				{
-					label: 'Settings',
-					icon: 'pi pi-cog',
-				},
-				{
-                    label: 'Logout',
-                    icon: 'pi pi-sign-out',
-                    to: '/'
-                }
-			]
-		}
-    },
-    methods: {
-        toggle(event) {
-            this.$refs.menu.toggle(event);
+  components: { Toolbar, Button, Menu },
+  data() {
+    return {
+      items: [
+        {
+          label: 'Active Campaigns',
+          icon: 'pi pi-flag'
         },
-        save() {
-            this.$toast.add({severity: 'success', summary: 'Success', detail: 'Data Saved', life: 3000});
+        {
+          label: 'Settings',
+          icon: 'pi pi-cog'
+        },
+        {
+          label: 'Logout',
+          icon: 'pi pi-sign-out',
+          to: '/'
         }
+      ]
+    };
+  },
+  methods: {
+    toggle(event) {
+      this.$refs.menu.toggle(event);
+    },
+    save() {
+      this.$toast.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Data Saved',
+        life: 3000
+      });
     }
-}
+  }
+};
 </script>
 
 <style>
 .logo {
-    width: 40pt;
+  width: 40pt;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Demo from '../views/Demo.vue';
+import Cart from '../views/Cart.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
   },
   {
     path: '/about',
@@ -15,28 +16,33 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/demo',
     name: 'Demo',
-    component: Demo,
+    component: Demo
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   },
   {
     path: '/get-started',
     name: 'Get started',
-    component: () => import('../views/Start.vue'),
+    component: () => import('../views/Start.vue')
   },
   {
     path: '/catalog',
     name: 'Catalog',
-    component: () => import('../views/Catalog.vue'),
-  },
+    component: () => import('../views/Catalog.vue')
+  }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes
 });
 
 export default router;
