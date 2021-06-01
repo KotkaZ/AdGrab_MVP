dc80200176499b3b3d8fa048e3e93dfa4fe5a86d
Kerdo Kurs <kerdokurs@gmail.com>
2021-06-01 12:36:55 +0300

    add basic getting started page

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="p-grid p-p-4">
-    <div class="p-col-1"></div>
-    <div class="p-col-3">
-      <Card>
-        <template #content> Telereklaam </template>
-      </Card>
+  <div id="get-started" class="h-screen p-p-3 p-m-0">
+    <div class="p-grid p-m-0 p-p-4 p-jc-center p-ai-center h-full">
+      <div class="p-col-3">
+        <Card>
+          <template #content>
+            <router-link to="/demo" class="link">Telereklaam</router-link>
+          </template>
+        </Card>
+      </div>
+      <div class="p-col-4">
+        <Card>
+          <template #content>
+            <router-link to="/demo" class="link">Trükimeedia</router-link>
+          </template>
+        </Card>
+      </div>
+      <div class="p-col-3">
+        <Card>
+          <template #content>
+            <router-link to="/demo" class="link"> Veebireklaam </router-link>
+          </template>
+        </Card>
+      </div>
     </div>
-    <div class="p-col-4">
-      <Card>
-        <template #content> Trükimeedia </template>
-      </Card>
-    </div>
-    <div class="p-col-3">
-      <Card>
-        <template #content> Veebireklaam </template>
-      </Card>
-    </div>
-    <div class="p-col-1"></div>
   </div>
 </template>
 
@@ -30,3 +36,22 @@ export default {
   setup() {},
 };
 </script>
+
+<style scoped>
+.h-screen {
+  height: 100vh;
+}
+
+.h-full {
+  height: 100%;
+}
+
+.link {
+  text-decoration: none;
+  color: var(--primary-color);
+}
+
+#get-started {
+  background-color: #e6e6e6;
+}
+</style>
